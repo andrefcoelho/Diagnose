a443aa618bd2042b1a518deed421080ebb95c5cd
andrefcoelho <andrefialhoc@gmail.com>
2018-11-26 20:57:14 -0200

    Update test_observer.m

--- test_observer.m
+++ test_observer.m
@@ -1,4 +1,4 @@
-%Example 2.21 Cassandras
+%% Example 2.21 Cassandras
 clear
 G=automaton('G');
 G.addState('0',1,1,{'a'},{'1'});
@@ -6,5 +6,10 @@ G.addState('1',0,0,{'b','b','epsl'},{'0','1','2'},[1 1 0]);
 G.addState('2',0,0,{'a','epsl'},{'0','3'});
 G.addState('3',0,0,{'b'},{'0'});
 Gobs=observer(G)
-% Gobs.unobservable={'b'}
-% Gobs1=observer(Gobs)
+
+%% Example to show markers
+Gobs.unobservable={'b'}
+Gobs1=observer(Gobs)
+%% Example 2 to show markers
+Gobs1.unobservable={'a'}
+Gobs2=observer(Gobs1)
